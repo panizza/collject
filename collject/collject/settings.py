e58692bc22f3c4f25a66a5fc4ab59ff14955aebb
jibbo <posta.giovanni.defrancesco@gmail.com>
2013-06-01 15:55:26 +0200

    fixed commas

--- collject/collject/settings.py
+++ collject/collject/settings.py
@@ -77,7 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    join(SETTINGS_PATH, "../app/static/"
+    join(SETTINGS_PATH, "../app/static/"),
 )
 
 # List of finder classes that know how to find static files in
@@ -117,7 +117,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    join(SETTINGS_PATH, "../app/templates/"
+    join(SETTINGS_PATH, "../app/templates/" ),
 )
 
 INSTALLED_APPS = (
