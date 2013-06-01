6d549491c3fb563ca0faadb2ae5cb4ccad4a17a4
Andrea Panizza <panizza01@gmail.com>
2013-06-01 22:33:38 +0200

    added jquery for debugging purpose

--- collject/api/urls.py
+++ collject/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url, include
 
 
 urlpatterns = patterns('api.views',
-    url(r'^problem/list/$', 'problem_list', name='problem_list'),
+    url(r'^problem/list/$', 'list_problem', name='list_problem'),
     url(r'^problem/(?P<problem_id>\d+)/info/$', 'get_problem_info', name='get_problem_info'),
     url(r'^problem/(?P<problem_id>\d+)/solutions/$', 'get_solution_of_problem', name='get_solution_of_problem'),
 )
--- collject/api/views.py
+++ collject/api/views.py
@@ -2,87 +2,72 @@ from django.shortcuts import render, get_object_or_404
 from basetyzer.models import Problem, Project, Solution
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count,Q
+from ajaxutils.decorators import ajax
+from django.db.models import Count, Q
 from .helpers import encode_json
 import json 
-from ajaxutils.decorator import ajax
-
-"""
-l√ista di tutti i problemi ordinata per follower
-√lista di tutti i progetti ordinata per follower
-√lista di tutte le soluzioni ordinata per +1
-
-dato un utente
- - lista di tutti i problemi ordinata per data last update (last_update generato da ultima modifica a una soluzione di quel problema)
- √ lista di tutti i progetti che followo o che ho creato
-
-
-√progetti -> lista di followers
-√idee -> lista di user che hanno fatto +1
-√problemi -> lista di follower
-"""
 
 @ajax(require="GET")
 def list_problem(request):
-	prbs = Problem.objects.annotate(follower_count=Count("follower")).order_by("-follower_count").values()
-	return encode_json(prbs.values('id','title','description'))
+    prbs = Problem.objects.annotate(follower_count=Count("follower")).order_by("-follower_count")
+    return encode_json(prbs.values('id', 'title', 'description', 'follower_count'))
 
 @ajax(require="GET")
 def get_problem_info(request, problem_id):
-	prob = get_object_or_404(Problem, pk=problem_id)
-	return encode_json(prob.values())
+    prob = get_object_or_404(Problem, pk=problem_id)
+    return encode_json(prob.values())
 
 @ajax(require="GET")
 def get_solution_of_problem(request, problem_id):
-	prob = get_object_or_404(Problem, pk=problem_id)
-	sols = prob.solution_set.all()
-	return encode_json(sols.values('id','description'))
+    prob = get_object_or_404(Problem, pk=problem_id)
+    sols = prob.solution_set.all()
+    return encode_json(sols.values('id', 'description'))
 
 @ajax(require="GET")
 def list_project(request):
-	prjs = Project.objects.annotate(follower_count=Count("follower")).order_by("-follower_count").values()
-	json_out= json.loads(encode_json(prjs.values('id','title','description','follower_count','creation_date','latitude','longitude')))
-	for obj in json_out:
-		obj['img'] = User.objects.get(pk=obj['owner_id']).get_profile().get_image_url()
-	return json_out
+    prjs = Project.objects.annotate(follower_count=Count("follower")).order_by("-follower_count").values()
+    json_out = json.loads(encode_json(prjs.values('id', 'title', 'description', 'follower_count', 'creation_date', 'latitude', 'longitude')))
+    for obj in json_out:
+        obj['img'] = User.objects.get(pk=obj['owner_id']).get_profile().get_image_url()
+    return json_out
 
 @ajax(require="GET")
 def get_project_info(request, project_id):
-	project = get_object_or_404(Project, pk=project_id)
-	return encode_json(project.values())
+    project = get_object_or_404(Project, pk=project_id)
+    return encode_json(project.values())
 
 @ajax(require="GET")
 def list_solution(request):
-	sols = Solution.objects.annotate(follower_count=Count("follower")).order_by("-follower_count").values()
-	return encode_json(prbs.values('id','problem_id','description','follower_count','creation_date'))
+    sols = Solution.objects.annotate(follower_count=Count("follower")).order_by("-follower_count").values()
+    return encode_json(prbs.values('id','problem_id','description','follower_count','creation_date'))
 
 @ajax(require="GET")
 def get_solution_info(request, solution_id):
-	sol = get_object_or_404(Solution, pk=solution_id)
-	return encode_json(sol.values())
+    sol = get_object_or_404(Solution, pk=solution_id)
+    return encode_json(sol.values())
 
 @login_required
 @ajax(require="GET")
 def list_my_project(request):
-	user= get_object_or_404(User, pk=request.user.id)
-	prjs = Project.objects.filter(Q(user=user)| Q(follower__in=[user]))
-	return encode_json(prbs.values('id','title','description','follower_count','creation_date'))
+    user= get_object_or_404(User, pk=request.user.id)
+    prjs = Project.objects.filter(Q(user=user)| Q(follower__in=[user]))
+    return encode_json(prbs.values('id','title','description','follower_count','creation_date'))
 
 @ajax(require="GET")
 def list_follower_of_project(request, project_id):
-	project = get_object_or_404(Project, pk=project_id)
-	flws = project.follower.all()
-	return encode_json(flws.values('id','email'))
+    project = get_object_or_404(Project, pk=project_id)
+    flws = project.follower.all()
+    return encode_json(flws.values('id','email'))
 
 @ajax(require="GET")
 def list_follower_of_solution(request, solution_id):
-	sol = get_object_or_404(Solution, pk=solution_id)
-	flws = sol.follower.all().values()
-	return encode_json(flws.values('id','email'))
+    sol = get_object_or_404(Solution, pk=solution_id)
+    flws = sol.follower.all().values()
+    return encode_json(flws.values('id','email'))
 
 @ajax(require="GET")
 def list_follower_of_problem(request, problem_id):
-	prob = get_object_or_404(Problem, pk=problem_id)
-	flws = prob.follower.all().values()
-	return encode_json(flws.values('id','email'))
+    prob = get_object_or_404(Problem, pk=problem_id)
+    flws = prob.follower.all().values()
+    return encode_json(flws.values('id','email'))
 
