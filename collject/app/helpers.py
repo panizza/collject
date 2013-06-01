5d3c694fb294188b6e9af911028ac790529e911f
jibbo <posta.giovanni.defrancesco@gmail.com>
2013-06-01 18:45:00 +0200

    fix encodert

--- collject/app/helpers.py
+++ collject/app/helpers.py
@@ -5,4 +5,4 @@ class MyJsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return datetime.strptime(obj, '%Y-%m-%d %H:%M:%S')
-        return super(MyJsonEncoder, self).default(obj)
+        return json.JSONEncoder.default(self,obj)
