6f4ef62995074f2f8c61b911b7a620e7d97591e4
Andrea Panizza <panizza01@gmail.com>
2013-06-02 17:12:25 +0200

    s

--- collject/main/static/js/script.js
+++ collject/main/static/js/script.js
@@ -15,9 +15,17 @@ $(document).ready(function(){
         $(this).toggleClass("open");
     });
     
+    function changeStyle(rel) {
+	    $("#style").attr("href", "static/css/" + rel + ".css");
+    }
+    
     $('nav li').on(event, function (e) {
         $("nav .active").removeClass("active");
         $(this).addClass("active");
+        $("#content > .list, .details, .form").hide();
+        rel = $(this).attr("rel");
+        $("." + rel).show();
+        changeStyle(id);
     });
     
     $('#content').on(event, function (e) {
@@ -29,7 +37,58 @@ $(document).ready(function(){
         $(this).addClass("active");
     });
     
-    $('.list section span.icon').on(event, function (e) {
-        $(this).toggleClass("follow");
+    $('span.follow').on(event, function (e) {
+        $(this).toggleClass("active");
+    });
+    
+    $('.form input').on('focus', function (e) {
+    	if(!$(this).hasClass("active"))
+    	{
+	    	$(this).val("");
+	        $(this).addClass("active");
+        }
+    });
+    
+    $('.form input').on('blur', function (e) {
+    	if($(this).val() == "")
+    	{
+    		$(this).val($(this).attr('aria-label'));
+        	$(this).removeClass("active");
+        }
+    });
+    
+    $('.form textarea').on('focus', function (e) {
+    	if(!$(this).hasClass("active"))
+    	{
+	    	$(this).html("");
+	        $(this).addClass("active");
+        }
+    });
+    
+    $('.form textarea').on('blur', function (e) {
+    	if($(this).html() == "")
+    	{
+    		$(this).html($(this).attr('aria-label'));
+        	$(this).removeClass("active");
+        }
+    });
+    
+    $('#content > .list > section, .problems .list > section, .solutions .list > section').on(event, function (e) {
+    	$("nav .active").removeClass("active");
+    	rel = $(this).attr("rel");
+        $("#" + rel).addClass("active");
+    	$("#content > .list, .details").hide();
+        $("." + rel).show();
+        changeStyle(rel);
+    });
+    
+    $('#back').on(event, function (e) {
+        $(".details, .form").hide();
+        $("#content > .list").show();
+    });
+    
+    $('#add').on(event, function (e) {
+        $("#content > .list, .details").hide();
+        $(".form").show();
     });
 });
